49677b3fb625f58e5aa43c83017bb67f682f9d89
quyang <quyang@focusmedia.cn>
2020-07-22 09:59:18 +0800

    2020-07-22：选楼方案

--- demo/big_he_pick_buildings.py
+++ demo/big_he_pick_buildings.py
@@ -13,6 +13,7 @@ class Greedy:
         self.max_epoch = config['max_epoch']
         self.max_sample = config['max_sample']
         self.building_limit = config['building_limit']
+        self.must_go = config['must_go']
 
     # how to choose buildings
     # 选楼函数
@@ -22,30 +23,55 @@ class Greedy:
         max_epoch = self.max_epoch
         max_sample = self.max_sample
         building_limit = self.building_limit
+        must_go = self.must_go
 
+        # 读取building+brand文件为一个dataframe，取名为df_building_brand
         df_building_brand = pd.read_excel(path)
+        # 把df_building_brand里的building_id作为index
         df_building_brand = df_building_brand.set_index(index)
+        # 新建一个set()集合,building_set用来存放挑中的楼
         building_set = set()
+        # 新建一个remain向量，存放11个为5的元素，并把最后一个元素化为0，作为已剩余数量向量的初始化
         remain = [building_limit] * df_building_brand.shape[1]
+        # 选取df_building_brand表中选中的楼（即必须取的楼）的行
+        rowdata = df_building_brand.loc[must_go]
+        # 把这些上述行的值相加
+        deletedata = np.sum(rowdata, axis=0)
+        # 更新remain
+        remain = [i - j for i, j in zip(remain, deletedata)]
         remain[-1] = 0
+        print(remain)
+        # 新建一个vector向量，作为品牌空缺向量的初始化（把remain向量进行init_vector()函数处理,再化成list，再化成array）
         vector = np.array(list(map(self.init_vector, remain)))
+        df_building_brand = df_building_brand.drop(must_go)
+        # print(df_building_brand)
+        # building_set.add(must_go.toSet)
+        for building in must_go:
+            building_set.add(building)
         j = 0
-
+        # 循环条件，品牌空缺（即有1）和循环次数不大于max_epoch
         while sum(vector) > 0 and j <= max_epoch:
+            # 把df_building_brand表和vector向量点积，并把结果表进行降序排序
             ser = df_building_brand.dot(vector).sort_values(0, ascending=False)
+            # print( df_building_brand.dot(vector))
             # print(ser)
+            # 把ser里得分为0 的删除
             ser = ser[ser > 0]
             if ser.size > 0:
+                # 最大得分，ser截取最大得分的行
                 max_value = ser.max()
                 ser = ser[ser == max_value]
-                sort = ser.head(max_sample).sample(
-                    n=1).index.tolist()[0]
-                # print(ser.size)
-                add = df_building_brand.ix[sort].values
+                # 截取ser最大得分里的前5行，在里面任选一行，再取该行的index
+                sort = ser.head(max_sample).sample(n=1).index.tolist()[0]
+                # 取df_building_brand表里的上述index对应的行值，并把最后一列即all值化为0
+                add = df_building_brand.loc[sort].values
                 add[-1] = 0
+                # 更新remain已剩余数量向量和vector品牌空缺向量
                 remain = [i - j for i, j in zip(remain, add)]
                 vector = np.array(list(map(self.init_vector, remain)))
+                # 在df_building_brand表里，把选中的index楼删除
                 df_building_brand.drop(sort, inplace=True)
+                # 把选中的楼加入building_set里
                 building_set.add(sort)
                 print("\n")
                 print("-" * 50 + "start" + "-" * 50)
@@ -60,6 +86,7 @@ class Greedy:
         return building_set, remain
 
     @staticmethod
+    # 定义的init_vector函数，大于0的化为1，其他为0
     def init_vector(x):
         result = 1 if x > 0 else 0
         return result
@@ -73,7 +100,9 @@ if __name__ == '__main__':
                    'max_epoch': 30,
                    'max_sample': 5,
                    'building_limit': 5,
-                   'plan_numbers': 10
+                   'plan_numbers': 10,
+                   'must_go': [3823, 4146, 12911, 17016, 18332, 63348, 72016, 80832, 2000109, 3000663, 3000771, 3009764,
+                               3030612, 3030704, 5003538, 5011982, 5018989, 5030449]
                    }
 
     # transform class to instance
@@ -86,6 +115,7 @@ if __name__ == '__main__':
     # 循环选楼
     for num in range(1, dict_config['plan_numbers']):
         print("#" * 50 + "开始方案" + str(num) + "#" * 50)
+        # 把module.pick()选楼函数l里返回的building_set和remain存入result_set和remain_set里
         (result_set, remain_set) = module.pick()
         list_result = list(result_set)
         list_result.sort()
