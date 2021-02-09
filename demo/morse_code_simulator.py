574f434016238cf7b0c49ce239b3d3ec6db0f4d8
quyang <quyang@focusmedia.cn>
2021-02-09 09:38:20 +0800

    2021-02-08：最近的代码

--- demo/morse_code_simulator.py
+++ demo/morse_code_simulator.py
@@ -21,6 +21,20 @@ pygame.mixer.init()
 channel = pygame.mixer.Channel(2)
 sound = pygame.mixer.Sound('../source/morse.wav')
 
+# constant
+timer_pop_character = 1
+timer_pop_space = 5
+timer_average_character = 2.2
+timer_threshold_character = 1.0
+timer_absolute_value = 0.5
+
+# output format
+output_start_format = '\033[1;30;46m'
+output_end_format = '\033[0m'
+
+# exception output
+output_exception_format = '\n\nThere is a typo! Restart~\n'
+
 
 # press listener
 def on_press(key):
@@ -44,10 +58,10 @@ def on_press(key):
         if len(space) > 1:
             max_space = max(space)
             min_space = min(space)
-            if max_space / min_space >= 2.2 or timestamps >= 1.0:
+            if max_space / min_space >= timer_average_character or timestamps >= timer_threshold_character:
                 decoder()
     except NameError:
-        print('\n\nThere is a typo! Restart~\n')
+        print(output_exception_format)
         character = []
         space = []
         input = []
@@ -67,9 +81,9 @@ def on_release(key):
     last_release_time = time.time()
     timestamps = last_release_time - last_press_time
     character.append(timestamps)
-    morse_timer = threading.Timer(1, decoder)
+    morse_timer = threading.Timer(timer_pop_character, decoder)
     morse_timer.start()
-    morse_space_timer = threading.Timer(5, plugin_space)
+    morse_space_timer = threading.Timer(timer_pop_space, plugin_space)
     morse_space_timer.start()
     return False
 
@@ -82,11 +96,11 @@ def plugin_space():
     input = []
     character = []
     space = []
-    format_code = '\033[1;30;46m' + str(' ') + '\033[0m'
+    format_code = output_start_format + str(' ') + output_end_format
     print(format_code, end='')
 
 
-# translate morse code into character
+# translate morse code into characters
 def decoder():
     global character
     global space
@@ -96,17 +110,17 @@ def decoder():
             input = []
         average_character = sum(character) / len(character)
         for value in character:
-            if value / average_character >= 2.2 or value >= 0.5:
+            if value / average_character >= timer_average_character or value >= timer_absolute_value:
                 input.append('-')
             else:
                 input.append('.')
         character = []
         space = []
         code = ''.join(input)
-        format_code = '\033[1;30;46m' + str(mtalk.decode(code)) + '\033[0m'
+        format_code = output_start_format + str(mtalk.decode(code)) + output_end_format
         print(format_code, end='')
     except KeyError:
-        print('\n\nThere is a typo! Restart~\n')
+        print(output_exception_format)
         character = []
         space = []
         input = []
